f6f16a80bee66157642c8c1ae12f97b109cb9959
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-03-22 13:05:44 -0700

    Setting up React app with AWS Amplify and Routing

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "notes-app-uploads-jd"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://3ndl8tl7c2.execute-api.us-west-2.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_65LRjE7nE",
+        APP_CLIENT_ID: "3hc2pj4ftq21210779mb7ua3on",
+        IDENTITY_POOL_ID: "us-west-2:75a4ef69-cfed-4c4c-b73c-5b8649a2df1b"
+    }
+};
