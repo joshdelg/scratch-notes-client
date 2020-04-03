df94ab471fe0217ee224023497793a6d4d07b4a7
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-04-03 10:37:16 -0700

    Added a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,17 +1,41 @@
-export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
     s3: {
         REGION: "us-west-2",
-        BUCKET: "notes-app-uploads-jd"
+        BUCKET: "notes-app-2-api-dev-attachmentsbucket-1bgjpdrj7jeix"
     },
     apiGateway: {
         REGION: "us-west-2",
-        URL: "https://3ndl8tl7c2.execute-api.us-west-2.amazonaws.com/dev"
+        URL: "https://h6edxj1kwi.execute-api.us-west-2.amazonaws.com/dev"
     },
     cognito: {
         REGION: "us-west-2",
-        USER_POOL_ID: "us-west-2_65LRjE7nE",
-        APP_CLIENT_ID: "3hc2pj4ftq21210779mb7ua3on",
-        IDENTITY_POOL_ID: "us-west-2:75a4ef69-cfed-4c4c-b73c-5b8649a2df1b"
+        USER_POOL_ID: "us-west-2_OSdpsSPep",
+        APP_CLIENT_ID: "7v0oqtumnte7qiabpag47u0ff1",
+        IDENTITY_POOL_ID: "us-west-2:709b4286-a255-48e1-b1d3-931eb0c47e24"
     }
+};
+
+const prod = {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "notes-app-2-api-prod-attachmentsbucket-1g45o9hps2xaq"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://nhs9sf2xx9.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_QnyWgVFae",
+        APP_CLIENT_ID: "3f1u540f18m983hcejqauuqvuj",
+        IDENTITY_POOL_ID: "us-west-2:19850b4a-0aa2-40f8-9d5d-6d5cd188d811"
+    }
+};
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod : dev;
+
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
